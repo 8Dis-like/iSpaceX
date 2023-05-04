2d5e38adcfc029d47f3ef35415cd7ec1e411ad42
Zugho <9796300+code-plateau@user.noreply.gitee.com>
2023-05-04 18:10:56 +0800

    the whole avaliable Huang-Ma version microprogram, in reference with the architecture

---
+++ Huang-Ma/app.js
@@ -0,0 +1,42 @@
+// app.js
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    //云环境启动
+    wx.cloud.init({
+      env:'data-1gplk8k9d9fd263f'
+    })
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+
+  globalData:{
+    userInfo:{ID:"", openid:"", wxnn:"", wxsrc:"", locationname:"", eventype:"", checktype:0,imgurl:[],mp4url:[], title:"", content:"", likes:0 , date:"", progressing:"", comment:[], personalsign:"这个家伙很懒，什么都没写下...",latitude:"",longitude:""},
+    loger:{openid:"", wxnn:"", wxsrc:"",personalsign:"",backimg:"",country:"",province:"",city:"",gender:"",job:"",phone:"",vitalphone:"",id:"",name:""},
+  }
+
+ 
+})
+
---
+++ Huang-Ma/cloud/msg_send/index.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+})
+//msg_send函数入口
+exports.main = async (event, context) => {
+  try {
+    const result = await cloud.openapi.subscribeMessage.send({
+        "touser": event.openid,
+        "page": 'login',
+        "lang": 'zh_CN',
+        "data": {
+          thing1: {value: event.theme},
+          date2: {value: event.startTime},
+          date3: {value: event.endTime},
+          thing4: {value: event.address}
+        },
+        "templateId": 'B43Xy25SJfhebP1MNaxnbqxy0opbYULjxhAlrMAAkww',
+        "miniprogramState": 'developer'
+      })
+    return result
+  } catch (err) {return err}
+}
