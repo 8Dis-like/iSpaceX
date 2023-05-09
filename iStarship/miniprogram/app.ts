5aca30a84f378fb1e1024220789f7d7f3e36d5ad
Zugho <9796300+code-plateau@user.noreply.gitee.com>
2023-05-09 11:04:17 +0800

    login works! along with cloudfunction: getid.

--- iStarship/miniprogram/app.ts
+++ iStarship/miniprogram/app.ts
@@ -1,8 +1,35 @@
 // app.ts
 App<IAppOption>({
-  globalData: {},
+  
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
   onLaunch() {
     //云开发环境初始化
     wx.cloud.init({env:"cloud1-0g77urib208cb8d9"})
       },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+  globalData: { 
+    userInfo:{ID:"", openid:"", wxnn:"", wxsrc:"", locationname:"", eventype:"", checktype:0,imgurl:[],mp4url:[], title:"", content:"", likes:0 , date:"", progressing:"", comment:[], personalsign:"这个家伙很懒，什么都没写下...",latitude:"",longitude:""},
+    loger:{openid:"", wxnn:"", wxsrc:"",personalsign:"",backimg:"",country:"",province:"",city:"",gender:"",job:"",phone:"",vitalphone:"",id:"",name:""},},
     })
--- iStarship/miniprogram/pages/index/index.ts
+++ iStarship/miniprogram/pages/index/index.ts
@@ -1,4 +1,30 @@
 Page({
+  data: {
+
+  },
+  onLoad() {
+    
+    wx.showToast({
+      
+      title: '成功',
+      icon: 'success',
+      duration: 700//持续的时间
+    })
+
+  },
+
+  to_letter(e){
+    wx.navigateTo({
+      url:'/pages/letter/letter?id=1'
+    })
+  },
+
+  to_instruction(e){
+    wx.navigateTo({
+      url:'/pages/instruction/instruction?id=1'
+    })
+  },
+  
   //云函数ADD
   add(){
     wx.cloud.callFunction({
